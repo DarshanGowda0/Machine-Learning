0b9b9275570af822f3ab18114dcc8729f7e2c2a2
darshan <darshan.gowda008@gmail.com>
2017-01-09 01:58:43 +0530

    handling missing datasets in R

--- Data Preprocessing/data_preprocessing.R
+++ Data Preprocessing/data_preprocessing.R
@@ -1,3 +1,15 @@
+
+
 #importing the dataset
 dataset = read.csv('Data.csv')
+
+#taking care of the missing data
+#if null, then take average else leave the same
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
 
