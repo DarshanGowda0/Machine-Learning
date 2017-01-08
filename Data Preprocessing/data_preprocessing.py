54186121bdcad4af26fc7bd17b70ac1e39bdb7cb
darshan <darshan.gowda008@gmail.com>
2017-01-09 01:44:04 +0530

    handling missing datasets in python

--- Data Preprocessing/data_preprocessing.py
+++ Data Preprocessing/data_preprocessing.py
@@ -7,5 +7,16 @@ import pandas as pd
 
 #importing the dataset
 dataset = pd.read_csv('Data.csv')
+
+#get all columns except last
 X = dataset.iloc[:,:-1].values
+
+#get the third indexed column
 y = dataset.iloc[:,3].values
+
+#taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN',strategy='mean',axis=0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
