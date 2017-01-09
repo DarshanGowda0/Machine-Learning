8ad2310baa66984e8f567a38a3bd30f085ff9594
darshan <darshan.gowda008@gmail.com>
2017-01-09 19:40:00 +0530

    added visualisation to training and test data in R

--- Regression/Simple Linear Regression/simple_linear_regression.R
+++ Regression/Simple Linear Regression/simple_linear_regression.R
@@ -20,3 +20,22 @@ regressor = lm(formula = Salary ~ YearsExperience,
 #predicting the result using the test data
 y_pred = predict(regressor,
                  newdata = test_set)
+
+
+#visualisations by plotting graphs
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot()+
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary, colour = 'red'))+
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor,newdata = training_set)),colour = 'blue')+
+  ggtitle('Experience vs Salary')+
+  xlab('Experience in years')+
+  ylab('Salaray')
+
+ggplot()+
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary, colour = 'red'))+
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor,newdata = training_set)),colour = 'blue')+
+  ggtitle('Experience vs Salary')+
+  xlab('Experience in years')+
+  ylab('Predicted Salaray')
+
