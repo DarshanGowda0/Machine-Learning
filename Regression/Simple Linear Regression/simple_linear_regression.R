2872bb914dc907873644e02d86a41d1e90a4e9a7
darshan <darshan.gowda008@gmail.com>
2017-01-09 18:53:46 +0530

    data preprocessing in R

---
+++ Regression/Simple Linear Regression/simple_linear_regression.R
@@ -0,0 +1,14 @@
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
