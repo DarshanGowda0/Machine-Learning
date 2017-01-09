6ae12fea5a9d0b3f4841d5d79966cbb1e9459f5f
darshan <darshan.gowda008@gmail.com>
2017-01-09 11:10:41 +0530

    handling categorical data in python

--- Data Preprocessing/data_preprocessing.py
+++ Data Preprocessing/data_preprocessing.py
@@ -20,3 +20,20 @@ imputer = Imputer(missing_values='NaN',strategy='mean',axis=0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
+#encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+#for encoding the text into single digit numbers
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+
+#for encoding text into binary digits, so that the ML algorithms dont take them into consideration
+#(usually done for independent variables)
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+#use label encoder for y, because its a dependant variable
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
+
+
