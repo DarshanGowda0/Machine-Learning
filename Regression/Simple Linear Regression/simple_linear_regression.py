cb06a2e3cb34de4217d27e25e60194d0b7aa39da
darshan <darshan.gowda008@gmail.com>
2017-01-09 18:01:08 +0530

    data preprocessing for simple linear regression in python

---
+++ Regression/Simple Linear Regression/simple_linear_regression.py
@@ -0,0 +1,25 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+
+#all rows and all columns except last (matrix)
+X = dataset.iloc[:, :-1].values
+
+#all rows and 1st indexed cloumn (vector)
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into 20 rows for training and 10 for test data
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Feature Scaling is not required for simple linear regression
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
