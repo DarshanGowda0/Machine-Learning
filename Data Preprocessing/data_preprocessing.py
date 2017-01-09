d7a893b8e1367a99a4d0de9035f4cfc67798812f
darshan <darshan.gowda008@gmail.com>
2017-01-09 17:48:18 +0530

    feature scaling in python

--- Data Preprocessing/data_preprocessing.py
+++ Data Preprocessing/data_preprocessing.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#importing the dataset
+#####importing the dataset
 dataset = pd.read_csv('Data.csv')
 
 #get all columns except last
@@ -14,13 +14,13 @@ X = dataset.iloc[:,:-1].values
 #get the third indexed column
 y = dataset.iloc[:,3].values
 
-#taking care of missing data
+#####taking care of missing data
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values='NaN',strategy='mean',axis=0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-#encoding categorical data
+#####encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 #for encoding the text into single digit numbers
@@ -36,6 +36,12 @@ X = onehotencoder.fit_transform(X).toarray()
 labelEncoder_y = LabelEncoder()
 y = labelEncoder_y.fit_transform(y)
 
-#seperating the training and test data
+#####seperating the training and test data
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_trian, y_test = train_test_split(X, y, test_size= 0.2, random_state = 0)
+
+#####feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
