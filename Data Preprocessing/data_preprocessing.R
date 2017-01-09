1d8321630733ed3580ff0fe1370d016fa99a8a47
darshan <darshan.gowda008@gmail.com>
2017-01-09 11:18:22 +0530

    handling categorical data in R

--- Data Preprocessing/data_preprocessing.R
+++ Data Preprocessing/data_preprocessing.R
@@ -13,3 +13,13 @@ dataset$Salary = ifelse(is.na(dataset$Salary),
                         ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                         dataset$Salary)
 
+#encoding categorical data
+#we use factors by default in R unlike python
+dataset$Country = factor(dataset$Country,
+                         levels = c('France','Spain','Germany'),
+                         labels = c(1,2,3))
+
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('Yes','No'),
+                           labels = c(1,0))
+
