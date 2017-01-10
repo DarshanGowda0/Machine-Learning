a23cc7fc986ebc819cc63ddb8da26fec01253fdd
darshan <darshan.gowda008@gmail.com>
2017-01-11 03:07:34 +0530

    changed the labels :P

--- Regression/Support Vector Regression/support_vector_regression.py
+++ Regression/Support Vector Regression/support_vector_regression.py
@@ -21,7 +21,7 @@ sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y)
 
-# Fitting the Regression Model to the dataset
+# Fitting the SVR Model to the dataset
 from sklearn.svm import SVR
 regressor = SVR(kernel='rbf')
 regressor.fit(X,y)
@@ -30,20 +30,20 @@ regressor.fit(X,y)
 #instead of 6.5, we need to scale(transform) the dataframe using standard scaler object & we also need to inverse_transform the result to get the result in the previous scale
 y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
 
-# Visualising the Regression results
+# Visualising the SVR results
 plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Regression Model)')
+plt.title('Truth or Bluff (SVR Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
-# Visualising the Regression results (for higher resolution and smoother curve)
+# Visualising the SVR results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Regression Model)')
+plt.title('Truth or Bluff (SVR Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
