55c69992ad06173a88bba36edc9a976b0a774d64
darshan <darshan.gowda008@gmail.com>
2017-01-11 01:35:03 +0530

    data preprocessing for polynomial regression in R

---
+++ Regression/Polynomial Linear Regression/polynomial_regression.R
@@ -0,0 +1,9 @@
+#dataset preprocessing
+
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+#splitting of dataset into training and test data is not required
+
+#feature scaling is also not required
+
