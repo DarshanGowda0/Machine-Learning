b84912404e13725a4e161dafa82ff53f2259b37f
darshan <darshan.gowda008@gmail.com>
2017-01-11 01:09:40 +0530

    added visualisations for polynomial and simple linear regression to compare the results

--- Regression/Polynomial Linear Regression/polynomial_regression.py
+++ Regression/Polynomial Linear Regression/polynomial_regression.py
@@ -19,9 +19,26 @@ lin_reg = LinearRegression()
 lin_reg.fit(X,y)
 
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 2)
+poly_reg = PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X)
 
 lin_reg2 = LinearRegression()
 lin_reg2.fit(X_poly ,y)
 
+#visualistaions for linear regression model
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Linear Regression')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+#visualisations for polynomial regression model
+X_grid = np.arange(max(X), min(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.title('Polynomial Regression')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
