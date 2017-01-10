47743af3ca84f861bd1f150bc93611a3e211181c
darshan <darshan.gowda008@gmail.com>
2017-01-11 00:44:57 +0530

    adding polynoial feature for dataset

--- Regression/Polynomial Linear Regression/polynomial_regression.py
+++ Regression/Polynomial Linear Regression/polynomial_regression.py
@@ -13,4 +13,15 @@ y = dataset.iloc[:, 2].values
 
 #no feature scaling as well
 
+#simple linear regression
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X,y)
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)
+X_poly = poly_reg.fit_transform(X)
+
+lin_reg2 = LinearRegression()
+lin_reg2.fit(X_poly ,y)
 
