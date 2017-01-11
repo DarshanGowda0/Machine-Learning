74f6cbb9a93150ba2de42c1e86c04246cb6ba1e7
darshan <darshan.gowda008@gmail.com>
2017-01-11 17:12:14 +0530

    Data preprocessing for logistic regression done

---
+++ 3 - Classification/Logistic Regression/logistic_regression.py
@@ -0,0 +1,20 @@
+# imooporting libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# importing dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, 4].values
+
+#splitting into training set and test set 
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+#feature scaling of dataset 
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
+
