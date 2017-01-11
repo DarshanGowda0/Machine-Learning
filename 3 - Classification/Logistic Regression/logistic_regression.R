8b236675178f60806e62db3b59c6af8f0d89b4aa
darshan <darshan.gowda008@gmail.com>
2017-01-11 18:18:39 +0530

    data preprocessing for logistic regression

---
+++ 3 - Classification/Logistic Regression/logistic_regression.R
@@ -0,0 +1,14 @@
+#importing dataset 
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[, 3:5]
+
+# splitting the data into training set and test set
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased,SplitRatio = 0.75)
+training_set = subset(dataset,split == TRUE)
+test_set = subset(dataset,split == FALSE)
+
+#feature scaling of independant variables
+training_set[, 1:2] = scale(training_set[, 1:2])
+test_set[, 1:2] = scale(test_set[, 1:2])
