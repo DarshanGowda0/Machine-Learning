4031cd184aa3beb50768a9d201dbdaf9522fd84a
darshan <darshan.gowda008@gmail.com>
2017-01-11 13:55:44 +0530

    Random forest regression implemented in R

--- 2 - Regression/6 - Random Forest Regression/random_forest_regression.py
+++ 2 - Regression/6 - Random Forest Regression/random_forest_regression.py
@@ -31,7 +31,7 @@ regressor.fit(X, y)
 y_pred = regressor.predict(6.5)
 
 # Visualising the Random Forest Regression results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
