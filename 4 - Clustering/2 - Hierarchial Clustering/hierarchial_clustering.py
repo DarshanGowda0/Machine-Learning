af5c20230b380b6fc56743456d3b2a7eda2ca461
darshan <darshan.gowda008@gmail.com>
2017-01-12 20:30:32 +0530

    Visualising clusters for AglomerativeClustering in Heirarchial Clustering

--- 4 - Clustering/2 - Hierarchial Clustering/hierarchial_clustering.py
+++ 4 - Clustering/2 - Hierarchial Clustering/hierarchial_clustering.py
@@ -21,3 +21,14 @@ from sklearn.cluster import AgglomerativeClustering
 hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
 y_hc = hc.fit_predict(X)
 
+#visualising the result 
+plt.scatter(X[y_hc == 0,0],X[y_hc == 0,1],s = 100, c = 'red', label = 'Careful')
+plt.scatter(X[y_hc == 1,0],X[y_hc == 1,1],s = 100, c = 'blue', label = 'Standard')
+plt.scatter(X[y_hc == 2,0],X[y_hc == 2,1],s = 100, c = 'green', label = 'Target')
+plt.scatter(X[y_hc == 3,0],X[y_hc == 3,1],s = 100, c = 'cyan', label = 'Careless')
+plt.scatter(X[y_hc == 4,0],X[y_hc == 4,1],s = 100, c = 'magenta', label = 'Sensible')
+plt.title('Cluster of Clients')
+plt.xlabel('income')
+plt.ylabel('spend')
+plt.legend()
+plt.show()
