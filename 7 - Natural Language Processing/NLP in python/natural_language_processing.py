d1b18cbb7581558689fa1947c18c25ec14ca7036
darshan <darshan.gowda008@gmail.com>
2017-01-14 03:02:31 +0530

    classification of bag of words using naive bayes method

--- 7 - Natural Language Processing/NLP in python/natural_language_processing.py
+++ 7 - Natural Language Processing/NLP in python/natural_language_processing.py
@@ -13,11 +13,14 @@ from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 corpus = []
 
+operators = set(('and', 'or', 'not'))
+stop = set(stopwords.words('english')) - operators
+
 for i in range(0, 1000):
     # review = review.lower().split()
     review = re.sub('[^a-zA-Z]',' ',dataset['Review'][i]).lower().split()
     ps = PorterStemmer()
-    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = [ps.stem(word) for word in review if not word in stop]
     review = ' '.join(review)
     corpus.append(review)
     
@@ -26,3 +29,19 @@ from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
