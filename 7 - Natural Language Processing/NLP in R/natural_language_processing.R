2584b5a68d599eed8f4da060e0407d6e115c96cb
darshan <darshan.gowda008@gmail.com>
2017-01-14 12:34:44 +0530

    removing stop words, stemming and stripping whitespaces

--- 7 - Natural Language Processing/NLP in R/natural_language_processing.R
+++ 7 - Natural Language Processing/NLP in R/natural_language_processing.R
@@ -5,9 +5,17 @@ dataset = read.delim('Restaurant_Reviews.tsv',quote = '',stringsAsFactors = FALS
 
 #cleaning the texts
 # install.packages('tm')
+# install.packages("SnowballC")
 library(tm)
+library(SnowballC)
 corpus = VCorpus(VectorSource(dataset$Review))
 corpus = tm_map(corpus, content_transformer(tolower))
 corpus = tm_map(corpus, removeNumbers)
 corpus = tm_map(corpus, removePunctuation)
+corpus = tm_map(corpus, removeWords, stopwords())
+corpus = tm_map(corpus, stemDocument)
+corpus = tm_map(corpus, stripWhitespace)
+
+
+
 
