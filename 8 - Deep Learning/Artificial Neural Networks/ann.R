0095643197ff037e4dd81ac7f4778808a739476f
darshan <darshan.gowda008@gmail.com>
2017-01-15 15:15:37 +0530

    compiling the ANN

--- 8 - Deep Learning/Artificial Neural Networks/ann.R
+++ 8 - Deep Learning/Artificial Neural Networks/ann.R
@@ -23,3 +23,25 @@ test_set = subset(dataset, split == FALSE)
 # Feature Scaling
 training_set[-11] = scale(training_set[-11])
 test_set[-11] = scale(test_set[-11])
+
+# Fitting ANN to the Training set
+ # install.packages('h2o')
+library(h2o)
+h2o.init(nthreads = -1)
+model = h2o.deeplearning(y = 'Exited',
+                         training_frame = as.h2o(training_set),
+                         activation = 'Rectifier',
+                         hidden = c(5,5),
+                         epochs = 100,
+                         train_samples_per_iteration = -2)
+
+
+# Predicting the Test set results
+y_pred = h2o.predict(model, newdata = as.h2o(test_set[-11]))
+y_pred = (y_pred > 0.5)
+y_pred = as.vector(y_pred)
+
+# Making the Confusion Matrix
+cm = table(test_set[, 11], y_pred)
+
+# h2o.shutdown()
