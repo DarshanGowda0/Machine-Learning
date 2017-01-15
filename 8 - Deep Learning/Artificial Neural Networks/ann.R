49e90562eb22818c750ab3241e93a14542b276a4
darshan <darshan.gowda008@gmail.com>
2017-01-15 14:55:18 +0530

    data preprocessing for ANN in R

---
+++ 8 - Deep Learning/Artificial Neural Networks/ann.R
@@ -0,0 +1,25 @@
+# Artificial Neural Network
+
+# Importing the dataset
+dataset = read.csv('Churn_Modelling.csv')
+dataset = dataset[4:14]
+
+# Encoding the categorical variables as factors
+dataset$Geography = as.numeric(factor(dataset$Geography,
+                                      levels = c('France', 'Spain', 'Germany'),
+                                      labels = c(1, 2, 3)))
+dataset$Gender = as.numeric(factor(dataset$Gender,
+                                   levels = c('Female', 'Male'),
+                                   labels = c(1, 2)))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Exited, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[-11] = scale(training_set[-11])
+test_set[-11] = scale(test_set[-11])
